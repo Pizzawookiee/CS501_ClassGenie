b815ef657334275336d735ad4a15d7bf6dcfb787
Pizzawookiee <47388918+Pizzawookiee@users.noreply.github.com>
2023-11-30 14:18:24 -0500

    moved OAuth code to fragment and viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,15 @@ dependencies {
         exclude ("org.apache.httpcomponents")
     }
 
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+
 
     implementation ("androidx.activity:activity:1.8.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.databinding:viewbinding:8.1.2")
 
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -66,6 +73,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/cs501_classgenie/OAuthFragment.kt
@@ -0,0 +1,60 @@
+package com.example.cs501_classgenie
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.cs501_classgenie.databinding.FragmentOAuthBinding
+import kotlinx.coroutines.launch
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+
+class OAuthFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = OAuthFragment()
+    }
+
+    val OAuthViewModel: OAuthViewModel by activityViewModels()
+
+    private var _binding: FragmentOAuthBinding? = null
+
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOAuthBinding.inflate(inflater, container, false)
+
+        val sync_button: Button = binding.syncButton
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                sync_button.setOnClickListener {
+                    //run OAuth code from ViewModel
+                    OAuthViewModel.authorize()
+                }
+            }
+        }
+
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
