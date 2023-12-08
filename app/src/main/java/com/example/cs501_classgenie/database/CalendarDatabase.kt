7c568b5660eb732a4ae040ad7d07e90bdae61f73
Pizzawookiee <47388918+Pizzawookiee@users.noreply.github.com>
2023-12-07 19:42:27 -0500

    cleaned up ViewModel

--- app/src/main/java/com/example/cs501_classgenie/database/CalendarDatabase.kt
+++ app/src/main/java/com/example/cs501_classgenie/database/CalendarDatabase.kt
@@ -7,6 +7,8 @@ import androidx.room.TypeConverters
 import com.example.cs501_classgenie.CalendarEvent
 import java.util.Date
 
+
+//Database for offline caching (allows for notifications to continue even if internet is down)
 @Database(entities = [ CalendarEvent::class ], version=1, exportSchema=false)
 @TypeConverters(EventTypeConverters::class)
 abstract class CalendarDatabase : RoomDatabase(){
